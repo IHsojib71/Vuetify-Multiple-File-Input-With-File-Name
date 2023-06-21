d08d61e3ee4d5c5326599f8943f6ec93826756e1
Md Ismail Hossain <ihsojib43@gmail.com>
2023-06-21 23:26:36 +0600

    ongoing component, need to make some changes

---
+++ VutifyMultipleFileInput.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <v-file-input  v-model="myfile" 
+                            :prepend-icon="null" 
+                            counter 
+                            dense 
+                            label="Select Files" 
+                            outlined
+                            prepend-inner-icon="mdi-paperclip" show-size multiple>
+                            <template v-slot:selection="{ text, index, file }">
+                                <v-chip small text-color="white" color="primary" close @click:close="removePhotoID(index)">
+                                    {{ text }}
+                                </v-chip>
+                            </template>
+         </v-file-input>
+
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        
+    },
+    data() {
+        return {
+            myfile : []
+        }
+    },
+   
+    methods:{
+        removePhotoID(index) {
+        this.myfile.splice(index, 1);
+        },
+    }
+
+};
+</script>
